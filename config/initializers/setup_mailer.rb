e91938713d1df7d571605fa4709ee1e36891c299
Marcelo Munhoz Pélos <mpelos@gmail.com>
2011-09-22 14:24:10 -0300

    As chaves do hash retornadas pelo YAML load devem ser string

--- config/initializers/setup_mailer.rb
+++ config/initializers/setup_mailer.rb
@@ -3,9 +3,9 @@ email = YAML.load_file("#{Rails.root}/config/credentials.yml")['email']
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => email[:domain],
-  :user_name            => email[:user],
-  :password             => email[:password],
+  :domain               => email['domain'],
+  :user_name            => email['user'],
+  :password             => email['password'],
   :authentication       => :plain,
   :enable_starttls_auto => true
 }
