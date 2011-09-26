2565c1014bcaf53804de2f51907a2165b27d7655
Marcelo Munhoz Pélos <mpelos@gmail.com>
2011-09-26 11:59:42 -0300

    Incluindo cidades Florianópolis e Brasília

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -6,13 +8,14 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-City.destroy_all
 
 cities = {}
-cities["Campinas"]  = "VCP"
-cities["Joinville"] = "JOI"
-cities["Recife"]    = "REC"
+cities["Brasília"]      = "BSB"
+cities["Campinas"]      = "VCP"
+cities["Florianópolis"] = "FLN"
+cities["Joinville"]     = "JOI"
+cities["Recife"]        = "REC"
 
 cities.each_pair do |name, code|
-  City.create(:name => name, :code => code)
+  City.find_or_create_by_name_and_code(name, code)
 end
