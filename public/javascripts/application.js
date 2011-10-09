508b7c9375c4fad7b6c9398736c3586a0ff1098d
Marcelo Munhoz Pélos <mpelos@gmail.com>
2011-10-09 20:17:52 -0300

    Melhorando usabilidade nos campos de data

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,21 @@
 jQuery(function($) {
   // jQUery-UI
   $('.datepicker').datepicker({
-    selectOtherMonths: true
+    selectOtherMonths: true,
+    onSelect:          function(dateText, inst) {
+                         var self = $('#' + inst.id);
+                         if($(self).data('order') == 1) {
+                           $('#travel_end_depart_date').attr('value', dateText);
+                         };
+
+                         if($(self).data('order') <= 2) {
+                           $('#travel_start_return_date').attr('value', dateText);
+                         };
+
+                         if($(self).data('order') <= 3) {
+                           $('#travel_end_return_date').attr('value', dateText);
+                         };
+                       }
   });
 
   // jQuery Time Entry
