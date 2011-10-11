66d454e59ab63073b2bac2d8c62214718bd41223
Marcelo Munhoz Pélos <mpelos@gmail.com>
2011-10-11 12:34:09 -0300

    Removendo gambiarra

--- lib/azul_watcher.rb
+++ lib/azul_watcher.rb
@@ -20,7 +20,7 @@ class AzulWatcher
       if @session.has_xpath? "//table[@class='info-table']"
         @session.find(:xpath, "//table[@class='info-table']").text.split("\nvoo ").drop(1).each do |text|
           localized_time = text.match(/\d{2}:\d{2}/).to_s
-          datetime = Time.utc(flight.date.year, flight.date.month, flight.date.day, localized_time[0, 2], localized_time[3, 2], 0) + 3.hours
+          datetime = Time.utc(flight.date.year, flight.date.month, flight.date.day, localized_time[0, 2], localized_time[3, 2], 0).to_formatted_s(:db)
           price = text.match(/\d+,\d{2}/).to_s.sub!(",", ".").to_f
 
           schedule = flight.schedules.find_or_create_by_datetime(datetime)
