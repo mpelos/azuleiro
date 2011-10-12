be535c529993b4ee228bee740effbec04c1ca87e
Marcelo Munhoz Pélos <mpelos@gmail.com>
2011-10-12 13:27:55 -0300

    Corrigindo query do método find_schedules_by_sql

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -59,7 +59,7 @@ class Travel < ActiveRecord::Base
     end
 
     def find_schedules_by_sql(origin_id, destination_id, minimum_datetime, maximum_datetime)
-      Schedule.find_by_sql "SELECT `schedules`.* FROM `schedules` INNER JOIN `prices` ON `schedules`.`id` = `prices`.`schedule_id` INNER JOIN `flights` ON `schedules`.`flight_id` = `flights`.`id` INNER JOIN `flights_travels` ON `flights`.`id` = `flights_travels`.`flight_id` INNER JOIN `travels` ON `travels`.`id` =  `flights_travels`.`travel_id` WHERE `travels`.`id` = #{id} AND `flights`.`origin_id` = #{origin_id} AND `flights`.`destination_id` = #{destination_id} AND `schedules`.`datetime` BETWEEN '#{minimum_datetime}' AND '#{maximum_datetime}' AND `prices`.`value` <> 0 ORDER BY `prices`.`value`"
+      Schedule.find_by_sql "SELECT `schedules`.* FROM `schedules` INNER JOIN `prices` ON `schedules`.`id` = `prices`.`schedule_id` INNER JOIN `flights` ON `schedules`.`flight_id` = `flights`.`id` INNER JOIN `flights_travels` ON `flights`.`id` = `flights_travels`.`flight_id` INNER JOIN `travels` ON `travels`.`id` =  `flights_travels`.`travel_id` WHERE `travels`.`id` = #{id} AND `flights`.`origin_id` = #{origin_id} AND `flights`.`destination_id` = #{destination_id} AND `schedules`.`datetime` BETWEEN '#{minimum_datetime.utc.to_formatted_s(:db)}' AND '#{maximum_datetime.utc.to_formatted_s(:db)}' AND `prices`.`value` <> 0 ORDER BY `prices`.`value`"
     end
 
     def price_modifier
