49bf96403bcd8e4b5ec4107f364707209c7a207d
Marcelo Munhoz Pélos <mpelos@gmail.com>
2011-10-17 11:45:52 -0200

    Código porco para atualização de campos de data

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,15 +4,22 @@ jQuery(function($) {
     selectOtherMonths: true,
     onSelect:          function(dateText, inst) {
                          var self = $('#' + inst.id);
-                         if($(self).data('order') == 1) {
+                         var formattedDateText = dateText.slice(6, 10) + dateText.slice(3, 5) + dateText.slice(0, 2);
+                         var endDepartDate = $('#travel_end_depart_date').attr('value');
+                         var formattedEndDepartDate = endDepartDate.slice(6, 10) + endDepartDate.slice(3, 5) + endDepartDate.slice(0, 2);
+                         var startReturnDate = $('#travel_start_return_date').attr('value');
+                         var formattedStartReturnDate = startReturnDate.slice(6, 10) + startReturnDate.slice(3, 5) + startReturnDate.slice(0, 2);
+                         var endReturnDate = $('#travel_end_return_date').attr('value');
+                         var formattedEndReturnDate = endReturnDate.slice(6, 10) + endReturnDate.slice(3, 5) + endReturnDate.slice(0, 2);
+                         if($(self).data('order') == 1 && formattedDateText > formattedEndDepartDate) {
                            $('#travel_end_depart_date').attr('value', dateText);
                          };
 
-                         if($(self).data('order') <= 2) {
+                         if($(self).data('order') <= 2 && formattedDateText > formattedStartReturnDate) {
                            $('#travel_start_return_date').attr('value', dateText);
                          };
 
-                         if($(self).data('order') <= 3) {
+                         if($(self).data('order') <= 3 && formattedDateText > formattedEndReturnDate) {
                            $('#travel_end_return_date').attr('value', dateText);
                          };
                        }
