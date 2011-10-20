3e5576b3e02832c962478d0ad0b9b5a6c7e21ad3
Marcelo Munhoz Pélos <mpelos@gmail.com>
2011-10-20 10:51:24 -0200

    Idiota

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,12 +2,12 @@
 class ApplicationMailer < ActionMailer::Base
   default :from => "verificador_azul@randomhost.com.br"
 
-  def user_waiting_for_approval
+  def user_waiting_for_approval(user)
     @user = user
     mail :to => User.administrators.collect(&:email), :subject => "#{user.email} aguardando aprovação"
   end
 
-  def user_approved
+  def user_approved(user)
     @user = user
     mail :to => user.email, :subject => "Sua conta foi aprovada"
   end
