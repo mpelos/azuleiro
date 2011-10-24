cd3a5024e0eb373007d9cee563b64a3377c6760c
Marcelo Munhoz Pélos <mpelos@gmail.com>
2011-10-23 22:35:19 -0200

    Inserindo todas as cidades

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,49 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
 
-
-cities = {}
-cities["Brasília"]      = "BSB"
-cities["Campinas"]      = "VCP"
-cities["Campo Grande"]  = "CGR"
-cities["Curitiba"]      = "CWB"
-cities["Florianópolis"] = "FLN"
-cities["Joinville"]     = "JOI"
-cities["Recife"]        = "REC"
-cities["Vitória"]       = "VIX"
+cities = {
+  'Aracaju'                 => 'AJU',
+  'Aracatuba'               => 'ARU',
+  'Belém'                   => 'BEL',
+  'Belo Horizonte'          => 'CNF',
+  'Brasília'                => 'BSB',
+  'Campinas'                => 'VCP',
+  'Campo Grande'            => 'CGR',
+  'Caxias do Sul'           => 'CXJ',
+  'Cuiabá'                  => 'CGB',
+  'Curitiba'                => 'CWB',
+  'Florianópolis'           => 'FLN',
+  'Fortaleza'               => 'FOR',
+  'Foz do Iguaçú'           => 'IGU',
+  'Goiania'                 => 'GYN',
+  'Ilhéus'                  => 'IOS',
+  'João Pessoa'             => 'JPA',
+  'Joinville'               => 'JOI',
+  'Juazeiro do Norte'       => 'JDO',
+  'Juiz de Fora'            => 'IZA',
+  'Maceió'                  => 'MCZ',
+  'Manaus'                  => 'MAO',
+  'Marília'                 => 'MII',
+  'Maringá'                 => 'MGF',
+  'Natal'                   => 'NAT',
+  'Navegantes'              => 'NVT',
+  'Palmas'                  => 'PMW',
+  'Porto Alegre'            => 'POA',
+  'Porto Seguro'            => 'BPS',
+  'Presidente Prudente'     => 'PPB',
+  'Recife'                  => 'REC',
+  'Ribeirão Preto'          => 'RAO',
+  'Rio de Janeiro'          => 'SDU',
+  'Rio de Janeiro - Galeão' => 'GIG',
+  'Salvador'                => 'SSA',
+  'São José do Rio Preto'   => 'SJP',
+  'São José dos Campos'     => 'SJK',
+  'São Luís'                => 'SLZ',
+  'São Paulo - Congonhas'   => 'CGH',
+  'Teresina'                => 'THE',
+  'Uberaba'                 => 'UBA',
+  'Vitória'                 => 'VIX'
+}
 
 cities.each_pair do |name, code|
   City.find_or_create_by_name_and_code(name, code)
