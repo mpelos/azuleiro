f311b9c29f8a2151d4f1e83edd1146240e34dd2d
Rodrigo Navarro <rnavarro1@gmail.com>
2011-10-27 21:21:09 -0200

    Argh(2)...

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -71,81 +71,82 @@ class Travel < ActiveRecord::Base
   end
 
   protected
-    def different_city_for_origin_and_destination
-      if origin == destination
-        errors.add :destination, "As cidades de origem e de destino devem ser diferentes"
+
+  def different_city_for_origin_and_destination
+    if origin == destination
+      errors.add :destination, "As cidades de origem e de destino devem ser diferentes"
+    end
+  end
+
+  def depart_date_range
+    if end_depart_datetime < start_depart_datetime
+      errors.add :depart_range, "A data máxima para partida deve ser superior à mínima"
+    end
+  end
+
+  def return_date_range
+    if end_return_datetime < start_return_datetime
+      errors.add :return_range, "A data máxima para retorno deve ser superior à mínima"
+    end
+  end
+
+  def depart_date_in_the_future
+    if end_depart_datetime < DateTime.current
+      errors.add :depart_range, "Sua disponibilidade de partida deve ser uma data futura"
+    end
+  end
+
+  def return_date_in_the_future
+    if end_return_datetime < DateTime.current
+      errors.add :return_range, "Sua disponibilidade de retorno deve ser uma data futura"
+    end
+  end
+
+  def depart_date_higher_then_return_date
+    if end_return_datetime < end_depart_datetime
+      errors.add :return_range, "Sua disponibilidade para viagem de retorno deve ser superior a de partida"
+    end
+  end
+
+  def maximum_depart_range
+    if ((end_depart_datetime - start_depart_datetime) / 3600).round > 72
+      errors.add(:depart_range, "Sua disponibilidade de viagem para partida deve ter no máximo 3 dias")
+    end
+  end
+
+  def maximum_return_range
+    if ((end_depart_datetime - start_depart_datetime) / 3600).round > 72
+      errors.add :return_range, "Sua disponibilidade de viagem para retorno deve ter no máximo 3 dias"
+    end
+  end
+
+  def maximum_travel_length_per_user
+    if user.travels.avaliable.length >= MAXIMUM_TRAVELS_PER_USER
+      errors.add :base, "Cadastrar #{MAXIMUM_TRAVELS_PER_USER} viagens não é o suficiente pra você?"
+    end
+  end
+
+  def set_null_to_return_datetime
+    self.start_return_datetime = nil
+    self.end_return_datetime   = nil
+  end
+
+  def find_or_create_flights
+    self.flights = []
+    depart_range = start_depart_datetime.to_date..end_depart_datetime.to_date
+    depart_range.each do |date|
+      flights << Flight.find_or_create_by_origin_id_and_destination_id_and_date(origin_id, destination_id, date)
+    end
+
+    if round_trip?
+      return_range = start_return_datetime.to_date..end_return_datetime.to_date
+      return_range.each do |date|
+        flights << Flight.find_or_create_by_origin_id_and_destination_id_and_date(destination_id, origin_id, date)
       end
     end
+  end
 
-    def depart_date_range
-      if end_depart_datetime < start_depart_datetime
-        errors.add :depart_range, "A data máxima para partida deve ser superior à mínima"
-      end
-    end
-
-    def return_date_range
-      if end_return_datetime < start_return_datetime
-        errors.add :return_range, "A data máxima para retorno deve ser superior à mínima"
-      end
-    end
-
-    def depart_date_in_the_future
-      if end_depart_datetime < DateTime.current
-        errors.add :depart_range, "Sua disponibilidade de partida deve ser uma data futura"
-      end
-    end
-
-    def return_date_in_the_future
-      if end_return_datetime < DateTime.current
-        errors.add :return_range, "Sua disponibilidade de retorno deve ser uma data futura"
-      end
-    end
-
-    def depart_date_higher_then_return_date
-      if end_return_datetime < end_depart_datetime
-        errors.add :return_range, "Sua disponibilidade para viagem de retorno deve ser superior a de partida"
-      end
-    end
-
-    def maximum_depart_range
-      if ((end_depart_datetime - start_depart_datetime) / 3600).round > 72
-        errors.add(:depart_range, "Sua disponibilidade de viagem para partida deve ter no máximo 3 dias")
-      end
-    end
-
-    def maximum_return_range
-      if ((end_depart_datetime - start_depart_datetime) / 3600).round > 72
-        errors.add :return_range, "Sua disponibilidade de viagem para retorno deve ter no máximo 3 dias"
-      end
-    end
-
-    def maximum_travel_length_per_user
-      if user.travels.avaliable.length >= MAXIMUM_TRAVELS_PER_USER
-        errors.add :base, "Cadastrar #{MAXIMUM_TRAVELS_PER_USER} viagens não é o suficiente pra você?"
-      end
-    end
-
-    def set_null_to_return_datetime
-      self.start_return_datetime = nil
-      self.end_return_datetime   = nil
-    end
-
-    def find_or_create_flights
-      self.flights = []
-      depart_range = start_depart_datetime.to_date..end_depart_datetime.to_date
-      depart_range.each do |date|
-        flights << Flight.find_or_create_by_origin_id_and_destination_id_and_date(origin_id, destination_id, date)
-      end
-
-      if round_trip?
-        return_range = start_return_datetime.to_date..end_return_datetime.to_date
-        return_range.each do |date|
-          flights << Flight.find_or_create_by_origin_id_and_destination_id_and_date(destination_id, origin_id, date)
-        end
-      end
-    end
-
-    def price_modifier
-      adults + (children * 0.6)
-    end
+  def price_modifier
+    adults + (children * 0.6)
+  end
 end
